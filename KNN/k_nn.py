692587a4e6b74a4bfcd25fa6749bf8fe2bc9e623
ISTC-CREATE LAB <istc@istc.com>
2018-07-13 19:59:33 +0400

    knn begin

--- KNN/k_nn.py
+++ KNN/k_nn.py
@@ -1,4 +1,9 @@
-def dist
+import numpy as np
+from collections import Counter
+
+def dist(a, b)
+    return np.sum(np.square(a-b))
+
 class K_NN:
     def __init__(self, k):
         """
@@ -10,13 +15,22 @@ class K_NN:
         """
         :param data: 3D array, where data[i, j] is i-th classes j-th point (vector: D dimenstions)
         """
-        # TODO: preprocessing
-        pass
+        self.data = np.asarray(data)
 
-    def predict(self, data):
+    def predict(self, X):
         """
         :param data: 2D array of floats N points each D dimensions
         :return: array of integers
         """
-        # TODO: predict
+        res=[]
+        for x in X:
+            dists = []
+            for i in range(self.data.shape[0]):
+                for d in self.data[i]:
+                    dists.append((dist(d, x), i))
+            nearests = [a[1] for a in sorted(dists)[:self.k]]
+            
+            counts = dict(Counter(nearests))
+            print(counts)
+#             res.append(
         return 0
